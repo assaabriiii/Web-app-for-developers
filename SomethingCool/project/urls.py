60a9c14ff09c381537cc0604b42ef4f746a57fa2
amirsbry <amirsbry1942@gmail.com>
2022-08-27 16:32:00 +0430

    intilized templates

--- SomethingCool/project/urls.py
+++ SomethingCool/project/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('' , views.mainPage , name="mainPage"),
     path('projects' , views.projects , name="projects"),
     path('<str:pk>' , views.projectPK , name='projectPK')
 ]
--- SomethingCool/project/views.py
+++ SomethingCool/project/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+projectList = [
+    {
+        "id" : 1 ,
+        "Title" : "SQL",
+        "Description" : "Stopped for a While"
+    },
+    {
+        "id" : 2 ,
+        "Title" : "CEH",
+        "Description" : "Training Myself"
+    },
+    {
+        "id" : 3 ,
+        "Title" : "Backend Programming",
+        "Description" : "Learning it"
+    }
+]
+
+def mainPage(request) :
+    return HttpResponse("The Main Page")
+
 def projects(request) :
-    return render(request , 'project/project.html')
+    page = "projects"
+    number = 11
+    context = {'page' : page , 'number' : number , 'projectList' : projectList} 
+    return render(request , 'project/project.html' , context)
 
 def projectPK(request , pk) :
     return render(request , "project/singleproject.html")
