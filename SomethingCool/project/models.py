ca05ad9079b62da82e4c1ff9873c3b985c9ec9d6
amirsbry <amirsbry1942@gmail.com>
2022-08-28 14:00:16 +0430

    backup for some banger actions

--- SomethingCool/project/models.py
+++ SomethingCool/project/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class project(models.Model):
+    id = models.UUIDField(default=uuid.uuid4 , unique=True , primary_key=True , editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True , blank=True)
+    demo_link = models.CharField(max_length=2000 , null=True , blank=True)
+    src_link = models.CharField(max_length=2000 , null=True , blank=True)
+    tags = models.ManyToManyField('Tag' , blank=True)
+    vote_total = models.IntegerField(default=0 , null=True , blank=True)
+    vote_ratio = models.IntegerField(default=0 , null=True , blank=True)
+
+    create_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) :
+        return self.title 
+        
+class review(models.Model) :
+    vote_type = (
+        ('up' , 'up vote'),
+        ('down' , 'down vote')
+    )
+    #Owner
+    Project = models.ForeignKey( project ,on_delete=models.CASCADE)
+    id = models.UUIDField(default=uuid.uuid4 , unique=True , primary_key=True , editable=False)
+    body = models.TextField(blank=True , null=True)
+    value = models.CharField(max_length=200 , choices=vote_type)
+    create_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) :
+        return self.value
+    
+    
+class Tag(models.Model) :
+    id = models.UUIDField(default=uuid.uuid4 , unique=True , primary_key=True , editable=False)
+    name = models.CharField(max_length=200)
+    create_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) :
+        return self.name
+     
