ac7d649817e8725d392278bc2ad56f7dffc711f6
amirsbry <amirsbry1942@gmail.com>
2022-08-30 20:49:17 +0430

    Added some style, Mostly cmd+c & cmd+v

--- SomethingCool/project/forms.py
+++ SomethingCool/project/forms.py
@@ -1,7 +1,20 @@
-from django.forms import ModelForm
+from django.forms import ModelForm , widgets
 from .models import project
-
+from django import forms
 class ProjectForm(ModelForm) :
-     class Meta :
-        model = project
-        fields = ['title' , 'description' , 'demo_link' , 'src_link' , 'tags' , 'fearured_img' ]
+   class Meta :
+      model = project
+      fields = ['title' , 'description' , 'demo_link' , 'src_link' , 'tags' , 'fearured_img' ]
+      widgets = {
+         'tags' : forms.CheckboxSelectMultiple(),
+      }
+   
+   def __init__(self , *args , **kwargs) :
+         super(ProjectForm , self).__init__(*args , **kwargs)
+         
+         # self.fields['title'].widget.attrs.update({'class' : 'input' , 'placeholder' : 'Add Title'})
+         # self.fields['description'].widget.attrs.update({'class' : 'input' , 'placeholder' : 'Add Description'})
+         
+         for name , field in self.fields.items() :
+            field.widget.attrs.update({'class' : 'input'})
+      
