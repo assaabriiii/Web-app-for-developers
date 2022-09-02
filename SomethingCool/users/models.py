7eca54b141649ae7fab52f0cd3929295895b87e4
amirsbry <amirsbry1942@gmail.com>
2022-09-02 22:59:08 +0430

    made user and profile conncection

--- SomethingCool/users/models.py
+++ SomethingCool/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+from django.db.models.signals import post_save,post_delete
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model) :
@@ -26,7 +28,7 @@ class Profile(models.Model) :
     
     
     def __str__(self) :
-        return self.user.username
+        return self.username
     
 
 class Skill(models.Model):
@@ -38,3 +40,4 @@ class Skill(models.Model):
     
     def __str__(self) :
         return self.name
+
